8d9e7597199654da1b8e633d0fcc29dea0f86de9
Dominique Barton <dbarton@confirm.ch>
2016-06-16 11:02:11 +0200

    Using new-style print

--- bit_positions.py
+++ bit_positions.py
@@ -28,4 +28,4 @@ import sys
 with open(sys.argv[1], 'r') as f:
     for l in f:
         n, p1, p2 = [int(s) for s in l.split(',')]
-        print str(n >> (p1 - 1) & 1 == n >> (p2 - 1) & 1).lower()
+        print(str(n >> (p1 - 1) & 1 == n >> (p2 - 1) & 1).lower())
