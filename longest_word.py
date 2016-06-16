dc2e6c642637ad3c5e240b46186edad169dda5ef
Dominique Barton <dbarton@confirm.ch>
2016-06-16 17:18:41 +0200

    Added solution for challenge Longest Word

---
+++ longest_word.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 Dominique Barton
+#
+# This file is part of Dominique Barton's CodeEval solutions.
+#
+# Dominique Barton's CodeEval solutions are free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# Dominique Barton's CodeEval solutions are distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dominique Barton's CodeEval solutions. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+'''
+Solution for the CodeEval challenge "Longest Word":
+https://www.codeeval.com/open_challenges/111/
+'''
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    for l in f:
+        x = ''
+        xl = 0
+        for w in (w.strip() for w in l.split(' ')):
+            wl = len(w)
+            if xl == 0 or wl > xl:
+                x  = w
+                xl = wl
+        print(x)
