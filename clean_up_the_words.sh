75c7ade0f61bdf791dba81e25ffc85d20d999bd1
Dominique Barton <dbarton@confirm.ch>
2016-06-18 03:18:45 +0200

    Added solution for challenge "Clean up The Words"

---
+++ clean_up_the_words.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright © 2016 Dominique Barton
+#
+# This file is part of Dominique Barton's CodeEval solutions.
+#
+# Dominique Barton's CodeEval solutions are free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# Dominique Barton's CodeEval solutions are distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dominique Barton's CodeEval solutions. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------------------
+#
+# Solution for the CodeEval challenge "Clean Up The Words":
+# https://www.codeeval.com/open_challenges/205/
+#
+cat $1 | tr '[[:upper:]]' '[[:lower:]]' | sed -r 's/[^a-z]/ /g;s/ +/ /g;s/^ //;s/ $//'
